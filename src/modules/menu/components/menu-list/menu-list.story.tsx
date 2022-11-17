e11781555c7d740e658c83e6ca981a63b1ddb002
Kaluna <kalinamono@gmail.com>
2022-11-17 13:08:05 +0200

    add props to menu-list

--- src/modules/menu/components/menu-list/menu-list.story.tsx
+++ src/modules/menu/components/menu-list/menu-list.story.tsx
@@ -1,14 +1,16 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import {MenuList} from './menu-list.component'
-
+import {MenuList} from './menu-list.component';
+import pizzaMenu from '@app/mocks/pizza.json';
 
 export default {
     title: 'Menu/Menu List',
     component: MenuList,
 } as ComponentMeta<typeof MenuList>;
 
-const Template: ComponentStory<typeof MenuList> = () => <MenuList/>;
+const Template: ComponentStory<typeof MenuList> = (args) => <MenuList{...args}/>;
 
 
 export const View = Template.bind({});
-
+View.args = {
+    items: pizzaMenu
+}
