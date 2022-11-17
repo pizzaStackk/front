725652a7122da8f39a019bc38330f8edb7576733
Kaluna <kalinamono@gmail.com>
2022-11-17 12:11:12 +0200

    change file location

---
+++ src/common/components/header/header.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+       <div className="h-12 shadow-xl px-6">
+            <div className="flex items-center h-full">
+                <a className='text-xl font-semibold' href="">🍕 PizzaStack</a>
+            </div>
+       </div>
+    );
+};
