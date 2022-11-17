9c41b3854bc15034912f196e49f69982f6bc8fed
Kaluna <kalinamono@gmail.com>
2022-11-17 14:54:41 +0200

    Create home page

--- src/common/components/footer/footer.component.tsx
+++ src/common/components/footer/footer.component.tsx
@@ -1,7 +1,7 @@
 import {FooterLink} from "@app/common/components/footer-link/footer-link.component";
 
 export const Footer = () => {
-    return <div className="bg-gray-900 pt-12 px-12 pb-8">
+    return <div className="bg-gray-900 p-12">
         <div>
             <div className="mb-4">
                 <span className="uppercase tex-bold text-sm text-zinc-500">Контакти</span>
--- src/common/components/header/header.component.tsx
+++ src/common/components/header/header.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Header = () => {
     return (
-       <div className="h-12 shadow-xl px-6">
+       <div className="h-12 shadow-xl px-6 mb-8">
             <div className="flex items-center h-full">
                 <a className='text-xl font-semibold' href="">🍕 PizzaStack</a>
             </div>
